78f45335e1bc75ba2f573121da4303e85c729022
lingxitong <2468371065@qq.com>
2024-07-09 16:00:58 +0800

    add

--- feature_extractor/CLAM/extract_features_fp.py
+++ feature_extractor/CLAM/extract_features_fp.py
@@ -2,29 +2,24 @@ import torch
 import torch.nn as nn
 from math import floor
 import os
-import random
-import numpy as np
-import pdb
 from torchvision import transforms
 import torch
-import numpy as np
-from tqdm import tqdm
 from typing import List, Union, Tuple
+from Ctranspath.ctrans import *
+from conch.open_clip_custom import *
 from torch.utils.data import DataLoader
-import PIL
 from transformers import CLIPModel, CLIPProcessor
 import timm
-from datasets import Dataset, Image
 import time
 from clam_datasets.dataset_h5 import Dataset_All_Bags, Whole_Slide_Bag_FP
 from torch.utils.data import DataLoader
 from models.resnet_custom import resnet50_baseline
 import argparse
-from utils.utils import print_network, collate_features
+from utils.utils import collate_features
 from utils.file_utils import save_hdf5
-from PIL import Image
 import h5py
 import openslide
+
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 def plip_transforms(pretrained=False):
@@ -53,9 +48,27 @@ def uni_transforms():
 )
     return uni_transform
 
-
-
+def conch_transforms():
+    conch_transform = transforms.Compose(
+	[	transforms.ToTensor(),
+		transforms.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), 
+                       		std=(0.26862954, 0.26130258, 0.27577711)),
+	])
     
+    return conch_transform
+
+def gig_transforms():
+	gig_transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean = (0.485, 0.456, 0.406),
+                                                                                std = (0.229, 0.224, 0.225))])
+	return gig_transform
+
+def ctranspath_transforms():
+    ctranspath_transform = transforms.Compose(
+	[	transforms.ToTensor(),
+		transforms.Normalize(mean = (0.485, 0.456, 0.406),std = (0.229, 0.224, 0.225))
+	])
+    return ctranspath_transform
+
 def load_plip_model( name: str,
                 device: Union[str, torch.device] = "cuda" if torch.cuda.is_available() else "cpu",
                 auth_token=None):
@@ -82,6 +95,12 @@ def compute_w_loader(args,file_path, output_path, wsi, model,
 		custom_transforms = plip_transforms(pretrained=pretrained)
 	elif args.backbone == 'uni':
 		custom_transforms = uni_transforms()
+	elif args.backbone == 'conch':
+		custom_transforms = conch_transforms()
+	elif args.backbone == 'ctranspath':
+		custom_transforms = ctranspath_transforms()
+	elif args.backbone == 'gig':
+		custom_transforms = gig_transforms()
 	else:
 		custom_transforms = None
      
@@ -104,6 +123,8 @@ def compute_w_loader(args,file_path, output_path, wsi, model,
 			# print(batch.shape)
 			if args.backbone == 'plip':
 				features = model.get_image_features(batch)
+			elif args.backbone == 'conch':
+				features = model.encode_image(batch)
 			else:
 				features = model(batch)
 			features = features.cpu().numpy()
@@ -125,7 +146,8 @@ parser.add_argument('--batch_size', type=int, default=256)
 parser.add_argument('--no_auto_skip', default=False, action='store_true')
 parser.add_argument('--custom_downsample', type=int, default=1)
 parser.add_argument('--target_patch_size', type=int, default=-1)
-parser.add_argument('--backbone', type=str,choices=['vit_s_imagenet','plip','uni','resnet50_imagenet','couch'])
+parser.add_argument('--backbone', type=str,choices=['vit_s_imagenet','plip','uni','resnet50_imagenet','conch','ctranspath','gig'])
+parser.add_argument('--model_dir', type=str, default=None, help='path to model directory')
 args = parser.parse_args()
 
 
@@ -133,6 +155,7 @@ if __name__ == '__main__':
 
 	print('initializing dataset')
 	csv_path = args.csv_path
+	model_dir = args.model_dir
 	if csv_path is None:
 		raise NotImplementedError
 
@@ -145,13 +168,11 @@ if __name__ == '__main__':
 
 	print('loading model checkpoint')
 	if args.backbone == 'resnet50_imagenet':
-
 		model = resnet50_baseline(pretrained=True)
 		model = model.to(device)
-	
 	elif args.backbone == 'plip':
-		model_name = '/path/to/your/plip'
-		model,_,_ = load_plip_model(name=model_name, auth_token=None)
+		model_dir = model_dir
+		model,_,_ = load_plip_model(name=model_dir, auth_token=None)
 		model = model.to(device)
 	elif args.backbone == 'vit_s_imagenet':
 		model = timm.create_model('vit_small_patch16_224', pretrained=True)
@@ -159,8 +180,24 @@ if __name__ == '__main__':
 		model = model.to(device)
 	elif args.backbone == 'uni':
 		model = timm.create_model("vit_large_patch16_224", img_size=224, patch_size=16, init_values=1e-5, num_classes=0, dynamic_img_size=True)
-		local_dir = '/path/to/your/uni'
+		local_dir = model_dir
 		model.load_state_dict(torch.load(os.path.join(local_dir, "pytorch_model.bin"), map_location=device), strict=True)
+		model = model.to(device)
+	elif args.backbone == 'conch':
+		local_dir = model_dir
+		checkpoint_path = os.path.join(local_dir, "pytorch_model.bin")
+		model, _ = create_model_from_pretrained("conch_ViT-B-16", checkpoint_path=checkpoint_path)
+		model = model.to(device)
+	elif args.backbone == 'ctranspaths':
+		local_dir = model_dir
+		checkpoint_path = os.path.join(local_dir, "ctranspath.pth")
+		model = ctranspath()
+		model.load_state_dict(torch.load(checkpoint_path, map_location=device), strict=True)
+		model = model.to(device)
+	elif args.backbone == 'gig':
+		local_dir = model_dir
+		checkpoint_path = os.path.join(local_dir, "pytorch_model.bin")
+		model = timm.create_model(checkpoint_path, pretrained=True)
 		model = model.to(device)
 
 	if torch.cuda.device_count() > 1:
