63c5ee11e2feecf4abda9437ab6e26eacc0e2e13
Toby <lingxt23@mails.tsinghua.edu.cn>
2024-07-21 19:39:56 +0800

    Update test_mil.py

--- test_mil.py
+++ test_mil.py
@@ -1,7 +1,8 @@
 import argparse
 from utils.yaml_utils import read_yaml
 from torch.utils.data import DataLoader
-from utils.loop_utils import val_loop,clam_val_loop,ds_val_loop
+from utils.loop_utils import val_loop,clam_val_loop,ds_val_loop,dtfd_val_loop
+from modules.DTFD_MIL.dtfd_mil import Classifier_1fc,Attention,DimReduction,Attention_with_Classifier
 import warnings
 from utils.wsi_utils import WSI_Dataset
 import torch
@@ -66,6 +67,58 @@ def test(args):
     
     print(f"Test log saved at: {test_log_path}")
     
+def dtfd_test(args):
+    yaml_path = args.yaml_path
+    print(f"MIL-model-yaml path: {yaml_path}")
+    yaml_args = read_yaml(yaml_path)
+    
+    test_dataset_csv = args.test_dataset_csv
+    print(f"Dataset csv path: {test_dataset_csv}")
+    test_ds = WSI_Dataset(test_dataset_csv,'test')
+    test_dataloader = DataLoader(test_ds,batch_size=1,shuffle=False)
+    
+    device = torch.device(f'cuda:{yaml_args.General.device}')
+    num_classes = yaml_args.General.num_classes
+    in_dim = yaml_args.Model.in_dim
+    classifier_dropout = yaml_args.Model.classifier_dropout
+    attCls_dropout = yaml_args.Model.attCls_dropout
+    act = yaml_args.Model.act
+    total_instance = yaml_args.Model.total_instance
+    num_Group = yaml_args.Model.num_Group
+    grad_clipping = yaml_args.Model.grad_clipping
+    distill = yaml_args.Model.distill
+    mdim = yaml_args.Model.mdim
+    numLayer_Res = yaml_args.Model.numLayer_Res
+    classifier = Classifier_1fc(mdim, num_classes, classifier_dropout).to(device)
+    attention = Attention(mdim).to(device)
+    dimReduction = DimReduction(in_dim, mdim, numLayer_Res=numLayer_Res).to(device)
+    attCls = Attention_with_Classifier(L=mdim, num_cls=num_classes, droprate=attCls_dropout).to(device)
+    criterion = get_criterion(yaml_args.Model.criterion)
+    
+    
+    model_list = [classifier,attention,dimReduction,attCls]
+    test_loss,test_metrics = dtfd_val_loop(device,num_classes,model_list,test_dataloader,criterion,num_Group,grad_clipping,distill,total_instance)
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    print('----------------INFO----------------\n')
+    print(f'{FAIL}Test_Loss:{ENDC}{test_loss}\n')
+    print(f'{FAIL}Test_Metrics:  {ENDC}{test_metrics}\n')
+    
+    test_log_dir = args.test_log_dir
+    os.makedirs(test_log_dir,exist_ok=True)
+    new_yaml_path = os.path.join(test_log_dir,f'Test_{model_name}.yaml')
+    shutil.copyfile(yaml_path,new_yaml_path)
+    new_test_dataset_csv_path = os.path.join(test_log_dir,f'Test_dataset_{yaml_args.Dataset.DATASET_NAME}.csv')
+    shutil.copyfile(test_dataset_csv,new_test_dataset_csv_path)
+    
+
+    test_log_path = os.path.join(test_log_dir,f'Test_Log_{model_name}.txt')
+    log_to_save = {'test_loss':test_loss,'test_metrics':test_metrics}
+    with open(test_log_path,'w') as f:
+        f.write(str(log_to_save))
+
+    
+    print(f"Test log saved at: {test_log_path}")
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -74,5 +127,11 @@ if __name__ == '__main__':
     parser.add_argument('--model_weight_path',type=str,default='/path/to/your/model-weight',help='path to model weights ')
     parser.add_argument('--test_log_dir',type=str,default='/path/to/your/test-log-dir',help='path to test log dir')
     args = parser.parse_args()
-    test(args)
+    yaml_path = args.yaml_path
+    yaml_args = read_yaml(yaml_path)
+    model_name = yaml_args.General.MODEL_NAME
+    if model_name == 'DTFD_MIL':
+        dtfd_test(args)
+    else:
+        test(args)
     
