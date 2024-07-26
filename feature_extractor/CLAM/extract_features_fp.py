599d89d1248728592fac58e2dac7b54bb867ad33
Toby <lingxt23@mails.tsinghua.edu.cn>
2024-07-27 00:17:55 +0800

    Update extract_features_fp.py

--- feature_extractor/CLAM/extract_features_fp.py
+++ feature_extractor/CLAM/extract_features_fp.py
@@ -52,11 +52,11 @@ def plip_transforms():
 	mean = (0.48145466,0.4578275,0.40821073)
 	std = (0.26862954,0.26130258,0.27577711)
 
-	transforms = transforms.Compose([
+	plip_transforms = transforms.Compose([
 					 transforms.ToTensor(),
 					 transforms.Normalize(mean = mean, std = std)])
 
-	return transforms
+	return plip_transforms
 
 def uni_transforms():
     uni_transform = transforms.Compose(
